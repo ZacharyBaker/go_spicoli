85f6f3585db1c9da6c0ba91c80508cca3ac45f03
Zach Baker <zach.r.baker1@gmail.com>
2020-03-19 07:56:10 -0700

    change to dummy api

--- main.go
+++ main.go
@@ -1,14 +1,58 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
-	"github.com/russross/blackfriday"
+	"github.com/gorilla/mux"
 )
+
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Introduction to Golang",
+		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+	},
+	{
+		ID:          "2",
+		Title:       "Spicoli",
+		Description: "tasty waves and a cool buzz",
+	},
+	{
+		ID:          "3",
+		Title:       "Three",
+		Description: "three three threee",
+	},
+	{
+		ID:          "4",
+		Title:       "Four",
+		Description: "four four four",
+	},
+}
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Welcome home!")
+	w.Write([]byte("Chicken boy"))
+}
+
+func spicoliHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hey bud, whats your problem")
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(events)
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,18 +61,8 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.GET("/mark", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
-	})
-
-	router.Run(":" + port)
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/events", getAllEvents).Methods("GET") // done
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
