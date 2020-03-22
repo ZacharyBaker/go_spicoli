3fb807ef4411c60cefaa5a010bffa58b280a2751
Zach Baker <zach.r.baker1@gmail.com>
2020-03-22 09:25:48 -0700

    remove logs

--- main.go
+++ main.go
@@ -38,7 +38,6 @@ type response struct {
 }
 
 func handleEvent(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("handleEvent running")
 	var e Event
 	err := json.NewDecoder(r.Body).Decode(&e)
 	if err != nil {
@@ -50,12 +49,6 @@ func handleEvent(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("☄ HTTP status code returned!"))
 
-	fmt.Println("after writing 200 status")
-
-	// send 200 response
-	// check if its the event you are looking for
-	// make a request with the headers below
-
 	if e.Event.Type == "app_mention" {
 		fmt.Println("app_mention is what is happening")
 		url := "https://slack.com/api/chat.postMessage"
@@ -63,18 +56,16 @@ func handleEvent(w http.ResponseWriter, r *http.Request) {
 		t := os.Getenv("BOT_TOKEN")
 		bt := "Bearer " + t
 
-		fmt.Println("e.Event.Channel", e.Event.Channel)
-
 		var jsonStr = []byte(fmt.Sprintf(`{"text":"Hey bud.", "channel": "%s"}`, e.Event.Channel))
 		req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("Authorization", bt)
 
-		fmt.Println("here is the request you are making:::", req)
+		_, err := client.Do(req)
 
-		resp, _ := client.Do(req)
-
-		fmt.Println("RESP:::", resp)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
